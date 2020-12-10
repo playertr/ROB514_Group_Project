0651ad1720d100eea8b36d27dbdcb4e8e902a160
Khushal Brahmbhatt <khushalbr@gmail.com>
2020-12-10 04:19:37 +0300

    main script first commit

--- Hat_following/main.py
+++ Hat_following/main.py
@@ -1,36 +1,63 @@
+import argparse
+import errno
+import numpy as np
+import os
 import time
-from .estimate_tag import PoseEst
+import yaml
+from threading import Thread
+
 from djitellopy import Tello
 
-# argparse if required
+from estimate_tag import PoseEst
+from move_drone import MoveDrone
+
+
+# parse args
+parser = argparse.ArgumentParser(
+    description='Specify file and videocapture options.',
+    formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+# parser.add_argument('--save', default=False, action='store_true', help='Save video stream to file.')
+parser.add_argument('-c', '--config', default='config.yaml', help='Input config file.')
+# parser.add_argument('-o', '--output', default=None, help='Specify filename to write to without the extension.')
+args = parser.parse_args()
+
 
 # load params from config.yaml
-rvec_min = [0,0,0]
-rvec_max = [0,0,0]
-tvec_min = [0,0,0]
-tvec_max = [0,0,0]
+config_file = args.config
+try:
+    with open(config_file) as f:
+        print(f'[INFO] Loading config file: {config_file}')
+        config = yaml.load(f)
+except IOError:
+    raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), config_file)
+
+theta_min = config["follow_tag"]["theta_min"] * np.pi / 180
+theta_max = config["follow_tag"]["theta_max"] * np.pi / 180
+offsets_min = config["follow_tag"]["offsets_min"]
+offsets_max = config["follow_tag"]["offsets_max"]
 
 
-# connect to Tello
-tello = Tello
+# initialize Tello
+tello = Tello()
 tello.connect()
 tello.streamoff()
-time.sleep(1)
+time.sleep(0.1)
 tello.streamon()
 
 
+pose_estimator = PoseEst(tello)
+move_drone = MoveDrone(tello)
 
-state_estimator = PoseEst(tello)
+while True:
+    theta = pose_estimator.get_arc_angle()[0]
+    _, offsets = pose_estimator.get_target_state()
 
-while True: # or while key to abort not pressed
-    rvec, tvec = state_estimator.read_image_pose()
-
-    if rvec_min < rvec < rvec_max:
-        if tvec_min < tvec < tvec_max:
-            continue
+    if theta_min < theta < theta_max:
+        if offsets_min < offsets < offsets_max:
+            move_drone.linear_control(0, 0, 0)
         else:
             pass
-            # x, y, z = calculate_xyz_error()
+            # vx, vy, vz = calculate_xyz_error()
             # go x y z speed
     else:
         pass
