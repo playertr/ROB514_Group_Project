8f8d3111c08ca74eaec5bbec4931a1b2dd24ed0b
Khushal Brahmbhatt <khushalbr@gmail.com>
2020-12-10 12:00:41 +0300

    modified linear control

--- Hat_following/main.py
+++ Hat_following/main.py
@@ -1,18 +1,16 @@
 import argparse
+import csv
 import errno
 import numpy as np
 import os
 import time
 import yaml
-import keyboard
 
 from djitellopy import Tello
+import keyboard
 
 from estimate_tag import PoseEst
 from move_drone import MoveDrone
-import csv
-import time
-import os
 
 LOGGING_INTERVAL = 0.1
 
@@ -42,7 +40,6 @@ offsets_max = np.array(config["follow_tag"]["offsets_max"])
 k_constant = config["follow_tag"]["k_constant"]
 
 
-
 # initialize Tello
 tello = Tello()
 tello.connect()
@@ -55,6 +52,7 @@ move_drone = MoveDrone(tello)
 
 move_drone.takeoff()
 
+
 # Create new log file 
 i=0
 while os.path.exists("log%s.csv" % i):
@@ -63,41 +61,41 @@ with open("log%s.csv" % i, "w") as csvfile:
     writer = csv.writer(csvfile)
     old_time = time.time()
 
-    while True:
-        pose_estimator.update(tello)  # Reads a single frame from the Tello and then updates the state estimation filter.
-        _, offsets = pose_estimator.get_target_state()
 
-        if keyboard.is_pressed('ctrl+q'):
-            move_drone.land()
-            break
+while True:
+    if keyboard.is_pressed('ctrl+q'):
+        move_drone.land()
+        break
 
-        if keyboard.is_pressed('ctrl+e'):
-            move_drone.emergency()
-            break
+    if keyboard.is_pressed('ctrl+e'):
+        move_drone.emergency()
+        break
 
-        pose_estimator.update(tello)  # Reads a single frame from the Tello and then updates the state estimation filter.
-        _, offsets = pose_estimator.get_target_state()
+    pose_estimator.update(tello)  # Reads a single frame from the Tello and then updates the state estimation filter.
+    _, offsets = pose_estimator.get_target_state()
+    vx, vy, vz = (0, 0, 0)
 
-        if offsets is None:
-            move_drone.linear_control(0, 0, 0)
-            continue
+    if offsets is None:
+        move_drone.linear_control(vx, vy, vz)
+        continue
 
-        offsets = offsets.flatten()
-        theta = pose_estimator.get_arc_angle()[0]
+    offsets = offsets.flatten()
+    theta = pose_estimator.get_arc_angle()[0]
+    print('Offsets: {}, Theta: {}'.format(offsets, theta))
 
-        if theta_min < theta < theta_max:
-            if (offsets_min < offsets).all() and (offsets < offsets_max).all():
-                move_drone.linear_control(0, 0, 0)
-            else:
-                vx, vy, vz = move_drone.calc_linear_vel(offsets, offsets_min[1], k_constant)
-                move_drone.linear_control(vx, vy, vz)
-
+    if theta_min < theta < theta_max:
+        if (offsets_min < offsets).all() and (offsets < offsets_max).all():
+            move_drone.linear_control(vx, vy, vz)
         else:
-            if theta < theta_min:
-                move_drone.arc_ccw()
-            elif theta > theta_max:
-                move_drone.arc_cw()
+            vx, vy, vz = move_drone.calc_linear_vel(offsets, offsets_min, offsets_max, k_constant)
+            print('Vx: {}, Vy: {}, Vz: {}'.format(vx, vy, vz))
+            move_drone.linear_control(vx, -vy, vz)
+    else:
+        if theta < theta_min:
+            move_drone.arc_ccw()
+        elif theta > theta_max:
+            move_drone.arc_cw()
 
-        if time.time() - old_time > LOGGING_INTERVAL:
-            writer.writerow([time.time(), offsets, vx, vy, vz])
-            old_time = time.time()
+    if time.time() - old_time > LOGGING_INTERVAL:
+        writer.writerow([time.time(), offsets, vx, vy, vz])
+        old_time = time.time()
--- Hat_following/move_drone.py
+++ Hat_following/move_drone.py
@@ -8,15 +8,16 @@
 
 # Import Tello package
 from djitellopy import Tello
-from math import pi,sin,cos
+from math import pi, sin, cos
 import time
 
+
 class MoveDrone:
-    def __init__(self,tello):
+    def __init__(self, tello):
         self.tello = tello
 
-    #The first few commands below are fairly straightforward, doesn't need much
-    #explanation
+    # The first few commands below are fairly straightforward, doesn't need much
+    # explanation
 
     def takeoff(self):
         self.tello.takeoff()
@@ -29,36 +30,36 @@ class MoveDrone:
 
     # This command uses the output of the linear control script to give rc
     # commands to the drone without any yaw
-    def linear_control(self,roll,thrust,pitch):
-        self.tello.send_rc_control(int(roll),int(pitch),int(thrust),0)
+    def linear_control(self, roll, thrust, pitch):
+        self.tello.send_rc_control(int(roll), int(pitch), int(thrust), 0)
 
     # This function calculates the x,y coordinate pairs in a circle
     # Used for arc calculations later
-    def points_in_circle(self,radius,velocity):
+    def points_in_circle(self, radius, velocity):
 
         points = []
 
         angle_iteration = 1
-        radian = (angle_iteration*pi)/180
+        radian = (angle_iteration * pi) / 180
 
         x_0 = radius
         y_0 = 0
-        points.append((x_0,y_0))
+        points.append((x_0, y_0))
 
-        time_step = (2.0*float(radius)/float(velocity))*sin(float(radian)/2.0)
+        time_step = (2.0 * float(radius) / float(velocity)) * sin(float(radian) / 2.0)
 
         for i in range(360):
-            point = i+1
-            x_i = radius*cos(point*radian)
-            y_i = radius*sin(point*radian)
-            points.append((round(x_i),round(y_i)))
+            point = i + 1
+            x_i = radius * cos(point * radian)
+            y_i = radius * sin(point * radian)
+            points.append((round(x_i), round(y_i)))
 
-        return points,time_step
+        return points, time_step
 
     # This function uses the circle function to calculae a portion of the
     # circle in an arc
-    def points_in_arc(self,radius,velocity,angle):
-        points,time_step = self.points_in_circle(radius, velocity)
+    def points_in_arc(self, radius, velocity, angle):
+        points, time_step = self.points_in_circle(radius, velocity)
 
         arc_points = []
 
@@ -66,41 +67,45 @@ class MoveDrone:
             position = i
 
             arc_points.append(points[position])
-        return arc_points,time_step
+        return arc_points, time_step
 
     # Using the arc points function, this allows the drone to move in an arc
     # with a given radius and angle
     ### note: yaw control has to be set experimentally, still working on it ###
-    def arc_control(self,points,time_step):
+    def arc_control(self, points, time_step):
         for i in points:
             x = i[0]
             y = i[1]
             # rc control here is rough, still need to figure out proper yaw control
-            self.tello.send_rc_control(x,y,0,10)
+            self.tello.send_rc_control(x, y, 0, 10)
             time.sleep(time_step)
         self.tello.stop()
 
     def arc_cw(self):
-        self.tello.send_rc_control(-40,0,0,40)
+        self.tello.send_rc_control(-40, 0, 0, 40)
 
     def arc_ccw(self):
-        self.tello.send_rc_control(40,0,0,-40)
+        self.tello.send_rc_control(40, 0, 0, -40)
 
     def stop(self):
-        self.tello.send_rc_control(0,0,0,0)
+        self.tello.send_rc_control(0, 0, 0, 0)
 
-    def calc_linear_vel(self, tvec, zoff_thresh, k):
-        """convert frame to cv2 image and show"""
-        if tvec is None:
-            xoff, yoff, zoff = [0, 0, 0]  # [0.001, 0.001, 0.001]
-        else:
-            xoff, yoff, zoff = [elem for elem in tvec.flatten()]
+    @staticmethod
+    def calc_linear_vel(self, tvec, tvec_min, tvec_max, k):
 
-        zoff = zoff - zoff_thresh  # distance to maintain from the target
+        vx, vy, vz = (0, 0, 0)
+        xoff, yoff, zoff = [elem for elem in tvec]
 
         # Control Equations, constants are adjusted as needed
-        Vx = k[0] * xoff
-        Vy = k[1] * yoff
-        Vz = k[2] * zoff
+        if not (tvec_min[0] < xoff < tvec_max[0]):
+            vx = k[0] * xoff
 
-        return Vx, Vy, Vz
+        if not (tvec_min[1] < yoff < tvec_max[1]):
+            vy = k[1] * yoff
+
+        if zoff < tvec_min[2]:
+            vz = k[2] * -zoff
+        elif zoff > tvec_max[2]:
+            vz = k[2] * zoff
+
+        return vx, vy, vz
